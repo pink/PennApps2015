a18036f1adc7652be8bd1a49301565b2725a4104
skallung <sean.kallungal@gmail.com>
2015-01-17 13:51:25 -0800

    Added phonegap file

---
+++ spoticrack/www/app.js
@@ -0,0 +1,97 @@
+// Ionic Starter App, v0.9.20
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'ionic.contrib.ui.tinderCards' is found in ionic.tdcards.js
+angular.module('starter', ['ionic', 'ionic.contrib.ui.tinderCards'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('menu', {
+      url: "/menu",
+      abstract: true,
+      templateUrl: "menu.html"
+    })
+    .state('menu.main', {
+      url: "/main",
+      views: {
+        'menuContent' :{
+          templateUrl: "main.html"
+        }
+      }
+    })
+    .state('menu.settings', {
+      url: "/settings",
+      views: {
+        'menuContent' :{
+          templateUrl: "settings.html"
+          //controller: "AttendeesCtrl"
+        }
+      }
+    })
+    .state('menu.about', {
+      url: "/about",
+      views: {
+        'menuContent' :{
+          templateUrl: "about.html"
+          //controller: "AttendeesCtrl"
+        }
+      }
+    })
+  
+  $urlRouterProvider.otherwise("/menu/main");
+})
+
+.directive('noScroll', function() {
+
+  return {
+    restrict: 'A',
+    link: function($scope, $element, $attr) {
+
+      $element.on('touchmove', function(e) {
+        e.preventDefault();
+      });
+    }
+  }
+})
+
+.controller('CardsCtrl', function($scope, TDCardDelegate) {
+  var cardTypes = [
+    { image: 'max.jpg' },
+    { image: 'ben.png' },
+    { image: 'perry.jpg' },
+    {image: 'red.jpg'}
+  ];
+
+  $scope.cardDestroyed = function(index) {
+    $scope.cards.splice(index, 1);
+  };
+
+  $scope.addCard = function() {
+    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+    newCard.id = Math.random();
+    $scope.cards.unshift(angular.extend({}, newCard));
+  };
+  
+  $scope.cards = [];
+  for(var i = 0; i < 3; i++) $scope.addCard();
+})
+
+.controller('CardCtrl', function($scope, TDCardDelegate) {
+  $scope.cardSwipedLeft = function(index) {
+    console.log('LEFT SWIPE');
+    $scope.addCard();
+  };
+  $scope.cardSwipedRight = function(index) {
+    console.log('RIGHT SWIPE');
+    $scope.addCard();
+  };
+})
+
+.controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
+  $scope.toggleLeft = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+});
