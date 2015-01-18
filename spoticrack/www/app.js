08a7814bdd3829a7845ee33254466dcda9ee69be
skallung <sean.kallungal@gmail.com>
2015-01-18 03:48:09 -0800

    did a lot

--- spoticrack/www/app.js
+++ spoticrack/www/app.js
@@ -4,14 +4,23 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'ionic.contrib.ui.tinderCards' is found in ionic.tdcards.js
+
+var flag = true;
+var card_scope;
+var audioObject = null;
 angular.module('starter', ['ionic', 'ionic.contrib.ui.tinderCards'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
+
     .state('spot-login', {
-      url: '/spot-login',
-      templateUrl: 'spot-login.html',
+      url: "/spot-login",
+      views: {
+        '': {
+          templateUrl: "spot-login.html"
+        }
+      }
     })
     .state('menu', {
       url: "/menu",
@@ -62,12 +71,11 @@ angular.module('starter', ['ionic', 'ionic.contrib.ui.tinderCards'])
 })
 
 .controller('CardsCtrl', function($scope, TDCardDelegate) {
+  card_scope = $scope;
   var cardTypes = [
-    { image: 'max.jpg', title: 'max' },
-    { image: 'ben.png', title: 'ben' },
-    { image: 'perry.jpg', title: 'perry' },
-    {image: 'red.jpg', title: 'red'}
+    {image: 'red.jpg', artist: 'Taylor Swift', title: 'I Knew You Were Trouble'}
   ];
+
 
   $scope.cards = [];
 
@@ -76,18 +84,22 @@ angular.module('starter', ['ionic', 'ionic.contrib.ui.tinderCards'])
     newCard.id = Math.random();
     $scope.cards.push(angular.extend({}, newCard));
   };
-  for(var i = 0; i < 3; i++) $scope.addCard();
+  for(var i = 0; i < 1; i++) $scope.addCard();
 
   $scope.cardSwipedLeft = function(index) {
     console.log('LEFT2 SWIPE');
+    $scope.cards.pop();
   };
-  $scope.cardSwipedRight = function(index) {
-    console.log('RIGHT SWIPE');
+  $scope.cardSwipedRight = function(index, card) {
+    addToMusic(card.id);
+    setSeed("artist", card.artist, card_scope);
+    $scope.cards.pop();
   };  
   $scope.cardDestroyed = function(index, card) {
-    $scope.cards.splice(index, 1);
+    $scope.cards.pop();
     console.log('Card Removed');
     console.log(card.title);
+    stop();
   };
 })
 
@@ -95,10 +107,15 @@ angular.module('starter', ['ionic', 'ionic.contrib.ui.tinderCards'])
   
 })
 
-.controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
+.controller('MainCtrl', function($scope, $ionicSideMenuDelegate, $ionicPopup, $timeout) {
+  if (flag == false){
+      flag=true;
+      showPopup($scope, $timeout, $ionicPopup);
+  }
   $scope.toggleLeft = function() {
     $ionicSideMenuDelegate.toggleLeft();
   };
+  // Triggered on a button click, or some other target
 });
 
 function toggle(source) {
@@ -106,4 +123,59 @@ function toggle(source) {
   for(var i=0, n=checkboxes.length;i<n;i++) {
     checkboxes[i].checked = source.checked;
   }
+  if (flag == true){
+    flag = false;
+  }
+  else{
+    flag = true;
+  }
+  console.log(flag);
 }
+
+function showPopup(scope, timeout, ionicPopup) {
+   scope.data = {}
+
+   // An elaborate, custom popup
+   var myPopup = ionicPopup.show({
+     template: '<div class="list"><label class="item item-radio"><input type="radio" name="group" ng-model="data.type" value="artist"><div class="item-content">Artist</div><i class="radio-icon ion-checkmark"></i></label><label class="item item-radio"><input type="radio" name="group" ng-model="data.type" value="genre"><div class="item-content">Genre</div><i class="radio-icon ion-checkmark"></i></label><label class="item item-input"><input type="text" ng-model="data.text" placeholder="Search..."></label></div>',
+     scope: scope,
+     buttons: [
+       {
+         text: '<b>Go</b>',
+         type: 'button-positive',
+         onTap: function(e) {
+          if (!scope.data.text) {
+             //don't allow the user to close unless he enters wifi password
+             e.preventDefault();
+           } else {
+             return scope.data;
+           }
+         }
+       },
+     ]
+   });
+   myPopup.then(function(res) {
+    setSeed(res.type, res.text, card_scope);
+    setTimeout(function(){console.log("BACK"); console.log(card_scope.cards);card_scope.$apply();}, 5500);
+   });
+   timeout(function() {
+      myPopup.close(); //close the popup after 3 seconds for some reason
+   }, 10000000);
+}
+
+
+function play(){
+  if (!audioObject){
+    console.log(card_scope.cards[card_scope.cards.length - 1].song);
+    audioObject = new Audio(card_scope.cards[card_scope.cards.length - 1].song);
+    audioObject.play();
+  }
+}
+
+function stop(){
+  if (audioObject){
+    audioObject.pause();
+    audioObject = null;
+  }
+}
+
--- spoticrack/www/js/spotify.js
+++ spoticrack/www/js/spotify.js
@@ -10,17 +10,21 @@ var main_url = "";
 
 var access_token = "";
 
-var artistOrGenre = "artist";
-var seedQuery = "The Killers";
+var artistOrGenre = "";
+var seedQuery = "";
 
+var scope;
+var songs = [];
+var artists = [];
+var songs_hold = [];
 // Login functions
 
 // 1. Authorize app with spotify
 function authorize() {
     var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
         '&response_type=token' +
-        '&scope=user-library-read' +
-        '&redirect_uri=' + encodeURIComponent("http://andrewhe.me");
+        '&scope=user-library-modify' +
+        '&redirect_uri=' + encodeURIComponent("http://andrewhe.me/spoticrack/www");
     document.location = url;
 }
 
@@ -34,52 +38,111 @@ function extractToken() {
     access_token = lol[1];
 }
 
-function setSeed(type, query) {
-    window.artistOrGenre = "artist"; // could be "album"
-    window.seedQuery = "The Killers";
+function addToMusic(songID) {
+    var url = "https://api.spotify.com/v1/me/tracks?ids=" + songID;
+    $.ajax({
+        url: url,
+        type: "PUT",
+        headers: {
+            "Authorization": "Bearer " + client_id,
+        },
+        success: function(response) {
+            console.log(response);
+        }
+    });
 }
 
-var songs = [];
+function setSeed(type, query, scope) {
+    artistOrGenre = type; // could be "album"
+    seedQuery = query;
+    console.log(artistOrGenre);
+    console.log(seedQuery);
+    this.scope = scope;
+    getTracks(5);
+}
 
 function getTracks(numTracks) {
     var url = "http://developer.echonest.com/api/v4" 
 	
     if (artistOrGenre === "artist") {
-	url += "/artist/";
+        url += "/artist/similar?results=10";
     }
     else {
-	url += "/genre/";
+        url += "/genre/artists?format=json";
     }
 
-    url+= "similar?api_key=" + echonest_key
-         + "&name=" + seedQuery
-         + "&format=json&results=" + numTracks;
-
+    url+= "&api_key=" + echonest_key
+         + "&name=" + seedQuery;
    
     $.getJSON(url, function(data) {
-	songs = [];
+        artists = [];
 
-	if (artistOrGenre === "artist") {
-            var artists = data["response"]["artists"];
-
-	    for (i=0; i<numTracks; i++) {
-     	        var url2 = "http://developer.echonest.com/api/v4/song/search?"
-		       + "api_key=" + echonest_key
-		       + "&artist=" + artists[i]["name"]
-               + "&bucket=id:spotify&bucket=track";
-            $.getJSON(url2, function(data) {
-                //console.log(data);
-                var randIndex = Math.floor(Math.random() * 14);
-                songs.push(data["response"]["songs"][randIndex]);
-            });
-	    }
-	   
-
+        artists = data["response"]["artists"];
+            /*
+            for (i=0; i<numTracks; i++) {
+                    var url2 = "http://developer.echonest.com/api/v4/song/search?"
+                   + "api_key=" + echonest_key
+                   + "&artist=" + artists[i]["name"]
+                   + "&bucket=id:spotify";
+                $.getJSON(url2, function(data) {
+                    //console.log(data);
+                    var randIndex = Math.floor(Math.random() * 14);
+                    songs.push(data["response"]["songs"][randIndex]);
+                    generateCard(data["response"]["songs"][randIndex]);
+                });
+            }
+            */
+        console.log(artists);
+        for (var i = 0; i < artists.length; i++){
+            searchAlbums(artists[i]["name"]);
         }
-	
-	setTimeout(function() { testSpotifyEchonest(); }, 1000);
+        setTimeout(function(){        for (var i = 0; i < artists.length; i++){
+            fetchTracks(songs[i]["id"], songs[i]["img"]);
+        }}, 2000);
 
+       
+        setTimeout(function(){ console.log(scope.cards); document.location= "#/menu/main"}, 3000);
+        
     });
+}
+var searchAlbums = function (query) {
+    $.ajax({
+        url: 'https://api.spotify.com/v1/search',
+        data: {
+            q: query,
+            type: 'album'
+        },
+        success: function (response) {
+            var num = Math.floor(Math.random()*4);
+            var img = response["albums"]["items"][num]["images"][0]["url"];
+            var id = response["albums"]["items"][0]["id"];
+            var album = {img: img, id: id};
+            songs.push(album);
+        }
+    });
+};
+
+var fetchTracks = function (albumId, url) {
+    $.ajax({
+        url: 'https://api.spotify.com/v1/albums/' + albumId,
+        success: function (data){
+            //console.log(data);
+            var num = Math.floor(Math.random()*data.tracks.items.length);
+            var artist = data.artists[0].name;
+            var song = data.tracks.items[num].preview_url;
+            var name = data.tracks.items[num].name;
+            var id = data.tracks.items[num].id;
+            var album = {id: id, image: url, artist: artist, song: song, title: name};
+            //console.log(album);
+            scope.cards.push(album);
+
+            songs_hold.push(album);
+        }
+    });
+};
+
+function generateCard(song) {
+    
 }
 
 function getImages(artist_name) {
@@ -87,12 +150,11 @@ function getImages(artist_name) {
         + "?api_key=" + echonest_key
         + "&name=" + artist_name
         + "&results=1";
-    $.getJSON(url, function(data){
+    $.getJSON(url, function(data) {
         console.log(data);
         var data_url = data["response"]["images"][0]["url"];
         console.log(data_url);
     });
-
 }
 
 function testSpotifyEchonest() {
